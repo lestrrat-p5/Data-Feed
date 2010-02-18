c71935fbdc86690248aae6ea275c42b5b7ff7557
lestrrat <lestrrat+github@gmail.com>
2010-02-18 19:37:03 +0900

    version++

--- lib/Data/Feed.pm
+++ lib/Data/Feed.pm
@@ -11,7 +11,7 @@ use DateTime::Format::ISO8601;
 
 use constant DEBUG => exists $ENV{DATA_FEED_DEBUG} ? $ENV{DATA_FEED_DEBUG} : 0;
 
-our $VERSION = '0.00010';
+our $VERSION = '0.00011';
 our $AUTHORITY = 'cpan:DMAKI';
 
 has 'parser' => (
